1757949f74375fff219992215688eab89f539f8c
Marc Tismonar <tismonarmarc@gmail.com>
2024-06-07 08:54:28 +0200

    Enemy "Skull Guy" added

--- Assets/game/Enemys/SkullGuy/Scripts/SkullGuy.cs
+++ Assets/game/Enemys/SkullGuy/Scripts/SkullGuy.cs
@@ -6,19 +6,63 @@ public class SkullGuy : MonoBehaviour
 {
     [Header("GameObjects")]
     private Rigidbody2D rb;
+    public Animator animator;
+
     [Header("Variables")]
-    public float scale = 0;
+    public float speed = 0;
+    public float timeDelayOnHit = 1.0f;
+    public bool isMovingRight = true;
+    bool isChangingDirection = false;
 
-
-    // Start is called before the first frame update
     void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator.SetInteger("AnimSpeedDecider", isMovingRight ? 1 : -1);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (Pause.IsPause)
+        {
+            rb.velocity = Vector2.zero;
+        }
+        else
+        {
+            if (isMovingRight && !isChangingDirection)
+            {
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+            }
+            else if (!isMovingRight && !isChangingDirection)
+            {
+                rb.velocity = new Vector2(-speed, rb.velocity.y);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            StartCoroutine(ChangeDirection());
+        }
         
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            LevelMgr.killPlayer = true;
+        }
+    }
+
+    IEnumerator ChangeDirection()
+    {
+        isChangingDirection = true;
+        rb.velocity = Vector2.zero;
+        
+        yield return new WaitForSeconds(timeDelayOnHit/2);
+
+        isMovingRight = !isMovingRight;
+        animator.SetInteger("AnimSpeedDecider", isMovingRight ? 1 : -1);
+
+        yield return new WaitForSeconds(timeDelayOnHit/2);
+        isChangingDirection = false;
     }
 }
